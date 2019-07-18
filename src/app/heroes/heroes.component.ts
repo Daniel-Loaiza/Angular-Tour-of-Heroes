385c083121bfa392cc045092a7212b7e95e1d917
Daniel Loaiza <danielloaizaisi@gmail.com>
2019-07-17 23:22:12 -0500

    Call it in ngOnInit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,8 +16,9 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
-
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
